4c0ac62a600b29feaf4b30d57862feeb074b203d
Moustafa Elsonbaty <elsonbaty@gmail.com>
2024-06-03 14:55:29 +0400

    create profile using supabase database

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,15 @@
 import { LuUser2 } from 'react-icons/lu';
-
-function UserIcon() {
+import { fetchProfileImage } from '@/utils/actions';
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        className='w-6 h-6 bg-primary rounded-full object-cover'
+      />
+    );
+  }
   return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
 export default UserIcon;
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,18 +1,52 @@
 'use server';
 
 import { profileSchema } from './schema';
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error('Please login to create a profile');
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: 'profile created' };
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
-    return { message: 'there was an error' };
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
   }
+  redirect('/');
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
 };
