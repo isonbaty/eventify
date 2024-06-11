0e5b1105fde8f1e3f913208cb8193fec8227668f
Moustafa Elsonbaty <elsonbaty@gmail.com>
2024-06-11 14:28:43 +0400

    userIcon update

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,17 +1,19 @@
 import { LuUser2 } from 'react-icons/lu';
 import { fetchProfileImage, fetchFirsName } from '@/utils/actions';
-
+import { Separator } from '../ui/separator';
 async function UserIcon() {
   const profileImage = await fetchProfileImage();
   const firstName = await fetchFirsName();
   if (profileImage) {
     return (
       <>
+        <Separator orientation='vertical' />
+
         <img
           src={profileImage}
           className='w-6 h-6 bg-primary rounded-full object-cover'
         />
-        <span>{firstName}</span>
+        <span className='text-xs'>{firstName}</span>
       </>
     );
   }
